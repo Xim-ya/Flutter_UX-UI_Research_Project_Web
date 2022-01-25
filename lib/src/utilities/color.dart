eb19983fee5c223a7512ddbb155cd08654592df7
Joo-esc <hj_lee_com@naver.com>
2022-01-25 10:48:00 +0900

    dev : svg경로 및 아이콘 추가

---
+++ lib/src/utilities/color.dart
@@ -0,0 +1,3 @@
+import 'dart:ui';
+
+const kDarkGrey = Color(0xFF4A5056);
--- lib/src/widgets/app_bar.dart
+++ lib/src/widgets/app_bar.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ux_research/src/utilities/color.dart';
 
 class HomeAppBar extends StatelessWidget with PreferredSizeWidget {
   final double appBarHeight = 100.0;
+
+  // Test Func
+  void tempFunc() {
+    print("TEST LOG");
+  }
+
   @override
   get preferredSize => Size.fromHeight(appBarHeight);
 
@@ -12,41 +20,45 @@ class HomeAppBar extends StatelessWidget with PreferredSizeWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           AppBar(
+            foregroundColor: kDarkGrey,
+            backgroundColor: Colors.transparent,
             automaticallyImplyLeading: false,
             elevation: 0,
             title: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Icon(Icons.search),
-                Icon(Icons.home),
-                PopupMenuButton<String>(
-                  icon: Icon(Icons.menu),
-                  itemBuilder: (BuildContext context) =>
-                      <PopupMenuEntry<String>>[
-                    PopupMenuItem<String>(
-                      value: "1",
-                      child: Text('Hello'),
-                    ),
-                    PopupMenuItem<String>(
-                      value: "2",
-                      child: Text('World'),
-                    ),
-                  ],
-                ),
-              ],
+              children: <Widget>[Icon(Icons.ac_unit_sharp), actionButtons()],
             ),
           ),
         ],
       ),
-      decoration: BoxDecoration(
-        boxShadow: <BoxShadow>[
-          BoxShadow(
-              color: Colors.black54,
-              blurRadius: 15.0,
-              offset: Offset(0.0, 0.75))
-        ],
-        color:
-            ThemeData.dark().appBarTheme.color ?? ThemeData.dark().primaryColor,
+    );
+  }
+
+  // AppBar 우측 버튼 리스트
+  Row actionButtons() {
+    return Row(
+      children: [
+        actionButton("Home", tempFunc()),
+        actionButton("About", tempFunc()),
+        actionButton("Contact", tempFunc()),
+        SvgPicture.asset("icons/search_ic.svg")
+      ],
+    );
+  }
+
+  // 우측 버튼
+  Container actionButton(String content, void tempFunc) {
+    return Container(
+      margin: const EdgeInsets.only(right: 50),
+      child: TextButton(
+        onPressed: () {
+          print("Hello");
+        },
+        child: Text(
+          content,
+          style: const TextStyle(
+              fontWeight: FontWeight.w300, fontSize: 16, color: kDarkGrey),
+        ),
       ),
     );
   }
